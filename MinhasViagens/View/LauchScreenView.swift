f4d5883f574149db42e070196dea291a0b6a250a
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-03-18 08:10:09 -0300

    [add] - Inicia projeto

---
+++ MinhasViagens/View/LauchScreenView.swift
@@ -0,0 +1,34 @@
+//
+//  LauchScreenView.swift
+//  MinhasViagens
+//
+//  Created by Jean Ramalho on 18/03/25.
+//
+import Foundation
+import UIKit
+
+class LauchScreenView:UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        
+    }
+    
+    private func setConstraints(){
+        
+    }
+}
